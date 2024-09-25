206f5f176d3d35dc56628e1e2236f0e38cebaec8
youngjun!!! <jrys7754@gmail.com>
2024-09-26 01:16:58 +0900

    chore : 오타 수정
    ScriptableObject 폴더 오타 수정

--- Assets/02_Scripts/Item/EquipmentItem.cs
+++ Assets/02_Scripts/Item/EquipmentItem.cs
@@ -25,5 +25,6 @@ public class EquipmentItem : Item
         return new EquipmentItem(Data as EquipmentItemData);
     }
 
-    //장비 아이템이 사용할 함수
+    //장비 아이템이 사용할 함수들이 이제 플레이어가 장비를 장착하거나 뺏을 때
+    //플레이어 스텟에서 더해주거나 빼줘야함
 }
