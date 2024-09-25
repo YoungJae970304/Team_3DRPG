e497ca0079ca7f8978d60dd47aa57c266e350038
YoungJae970304 <sksdl1643020@gmail.com>
2024-09-25 15:36:33 +0900

    feat: 플레이어 피격 구현중, 플레이어 공격 구현중 / fix: 회피 이상 수정

--- Assets/02_Scripts/Controllers/Player/PlayerController/MeleePlayer.cs
+++ Assets/02_Scripts/Controllers/Player/PlayerController/MeleePlayer.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class MeleePlayer : Player
 {
-    public override void Attack()
+    public override IEnumerator Attack()
     {
         Debug.Log("근거리 캐릭터 공격");
+        switch (AtkCount)
+        {
+            case 0:
+                Logger.Log("강공격");
+                break;
+            case 1:
+                Logger.Log("기본공격 1타");
+                break;
+            case 2:
+                Logger.Log("기본공격 2타");
+                break;
+            case 3:
+                Logger.Log("기본공격 3타");
+                break;
+            default:
+                Logger.LogError("지정한 공격이 아님");
+                break;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        _attacking = false;
     }
 }
