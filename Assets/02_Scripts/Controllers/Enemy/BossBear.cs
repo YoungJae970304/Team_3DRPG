d45ca79f86714cb39ce455836c98f2ad1b9fa77f
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-09-29 12:27:01 +0900

    refactor: 몬스터 상태 조건 수정
    정상작동확인

--- Assets/02_Scripts/Controllers/Enemy/BossBear.cs
+++ Assets/02_Scripts/Controllers/Enemy/BossBear.cs
@@ -9,7 +9,7 @@ public class BossBear : Monster, IDamageAlbe
     // Start is called before the first frame update
     void Start()
     {
-  
+        
     }
 
     // Update is called once per frame
