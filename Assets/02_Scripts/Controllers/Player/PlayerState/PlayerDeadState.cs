b41524c2ba28aadedf3c409eeb9cb72cca6ed3f8
YoungJae970304 <sksdl1643020@gmail.com>
2024-10-01 15:29:47 +0900

    feat: 사망 상태 추가, 원거리 및 근거리 캐릭터 프리팹화

--- Assets/02_Scripts/Controllers/Player/PlayerState/PlayerDeadState.cs
+++ Assets/02_Scripts/Controllers/Player/PlayerState/PlayerDeadState.cs
@@ -7,16 +7,32 @@ public class PlayerDeadState : BaseState
     public PlayerDeadState(Player player, Monster monster, Stat stat) : base(player, monster, stat) { }
     public override void OnStateEnter()
     {
-
+        Logger.Log("플레이어 사망");
     }
 
     public override void OnStateUpdate()
     {
-
+        Logger.Log("사망상태 Update");
+        // 임시로 1.5초후 비활성화
+        DeadTimer(1.5f);
     }
 
     public override void OnStateExit()
     {
+        Logger.Log("사망상태 Exit");
+    }
 
+
+    float curtime = 0;
+    void DeadTimer(float targetTime)
+    {
+        curtime += Time.deltaTime;
+
+        if (curtime > targetTime)
+        {
+            curtime = 0;
+
+            _player.gameObject.SetActive(false);
+        }
     }
 }
