356e53d25031a0bb894171376f57ca36c7eb92a9
YoungJae970304 <sksdl1643020@gmail.com>
2024-10-05 09:38:42 +0900

    feat: 임시로 이동관련 애니메이션 추가

--- Assets/02_Scripts/Controllers/Player/PlayerState/PlayerMoveState.cs
+++ Assets/02_Scripts/Controllers/Player/PlayerState/PlayerMoveState.cs
@@ -10,11 +10,33 @@ public class PlayerMoveState : BaseState
     public override void OnStateEnter()
     {
         _player.AtkCount = 0;
+        _player._playerAnim.SetBool("Run", true);
+
+        switch (_player._playerCam._cameraMode)
+        {
+            case Define.CameraMode.QuarterView:
+                _player._playerAnim.SetBool("ZoomMode", false);
+                break;
+
+            case Define.CameraMode.ZoomView:
+                _player._playerAnim.SetBool("ZoomMode", true);
+                break;
+        }
     }
 
     public override void OnStateUpdate()
     {
-        
+        switch (_player._playerCam._cameraMode)
+        {
+            case Define.CameraMode.QuarterView:
+                _player._playerAnim.SetFloat("PosX", _player._cc.velocity.magnitude);
+                break;
+
+            case Define.CameraMode.ZoomView:
+                _player._playerAnim.SetFloat("PosX", _player._rotDir.x);
+                _player._playerAnim.SetFloat("PosY", _player._rotDir.z);
+                break;
+        }
     }
 
     public override void OnStateFixedUpdate()
@@ -61,5 +83,6 @@ public class PlayerMoveState : BaseState
     void MoveStop()
     {
         _player._moveDir = Vector3.zero;
+        _player._playerAnim.SetBool("Run", false);
     }
 }
