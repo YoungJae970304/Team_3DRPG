ad5ce53bbc18378610d0f26e8b77655dfb6852cf
youngjun-111 <jrys7754@gmail.com>
2024-10-07 13:59:32 +0900

    chore : 획득 예외 처리 진행중

--- Assets/02_Scripts/Item/ItemPickup.cs
+++ Assets/02_Scripts/Item/ItemPickup.cs
@@ -8,7 +8,7 @@ public class ItemPickup : MonoBehaviour
     // itemID를 string으로 받아옴
     public string _itemId;
     [SerializeField] float _pickupDuration = 1f;
-
+    Tweener _tweener;
     private void Awake()
     {
         _inventory = FindAnyObjectByType<Inventory>();
@@ -17,24 +17,18 @@ public class ItemPickup : MonoBehaviour
     private void Start()
     {
         _player = Managers.Game._player.transform;
-        TryPickupItem();
+        TryPickupItemEffect();
         if (_player == null)
         {
             Logger.LogError("플레이어가 없음");
         }
     }
 
-    //드랍된 아이템의 정보를 가져올 함수
-    public void GetDropItemID(DeongeonLevel level)
-    {
-        //_itemId = Drop._drop.DropItemSelect(level);
-    }
-
-    void TryPickupItem()
+    void TryPickupItemEffect()
     {
         //float distance = Vector3.Distance(transform.position, _player.position);
         Renderer renderer = GetComponent<Renderer>();
-      
+
         Sequence seq = DOTween.Sequence();
         //transform.position = Vector3.MoveTowards(transform.position, _player.position, _pickupSpeed * Time.deltaTime);
         //플레이어에게 이동
@@ -43,34 +37,49 @@ public class ItemPickup : MonoBehaviour
         seq.Join(transform.DOScale(Vector3.zero, _pickupDuration - 0.1f).SetEase(Ease.InBack));
         //이동하면서 점점 하얘짐
         seq.Join(renderer.material.DOColor(Color.white, _pickupDuration));
-        //시퀀스 완료 후 아이템 획득
-        seq.OnComplete(PickupItem);
-    }
-
-    void PickupItem()
-    {
-        if (!string.IsNullOrEmpty(_itemId))
+        //근데 시퀀스가 종료되기전에 플레이어가 움직이면 플레이어를 향해서 다시 경로 재탐색 
+        if (Vector3.Distance(transform.position, _player.position) < 0.1f)
         {
-            // string을 int로 변환
-            if (int.TryParse(_itemId, out int itemID))
+            //연출 효과 종료하고 경로 재 탐색
+            //Kill = Complete를 펄스로 해버림
+            _tweener.Kill();
+            FollowPlayer();
+            //시퀀스 종료.
+            seq.Complete();
+        }
+
+        void FollowPlayer()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _player.position, 10f * Time.deltaTime);
+            PickupItem();
+        }
+
+        void PickupItem()
+        {
+
+            if (!string.IsNullOrEmpty(_itemId))
             {
-                // id를 전달
-                _newItem = Item.ItemSpawn(itemID);
-                if (_newItem != null) // null 체크
+                // string을 int로 변환
+                if (int.TryParse(_itemId, out int itemID))
                 {
-                    Logger.Log("아이템 생성");
-                    if (_inventory != null)
+                    // id를 전달
+                    _newItem = Item.ItemSpawn(itemID);
+                    if (_newItem != null) // null 체크
                     {
-                        _inventory.InsertItem(_newItem);
-                        Logger.Log($"{_newItem.Data.ID} 인벤토리에 추가");
-                    }
-                    else
-                    {
-                        Logger.Log("인벤토리에 못넣음");
+                        Logger.Log("아이템 생성");
+                        if (_inventory != null)
+                        {
+                            _inventory.InsertItem(_newItem);
+                            Logger.Log($"{_newItem.Data.ID} 인벤토리에 추가");
+                        }
+                        else
+                        {
+                            Logger.Log("인벤토리에 못넣음");
+                        }
                     }
                 }
             }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }
 }
