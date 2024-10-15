207a9fe2b260abe04f3caddadb3bcc88fb4c34d0
youngjun!!! <jrys7754@gmail.com>
2024-10-15 23:07:13 +0900

    chore : 플레이어 인풋 관리필요

--- Assets/02_Scripts/UI/New Folder/DialogDungeonUI.cs
+++ Assets/02_Scripts/UI/New Folder/DialogDungeonUI.cs
@@ -8,9 +8,11 @@ public class DialogDungeonUI : MonoBehaviour
     DialogSystem[] _dialogSystem;
     public GameObject _dungeonUI;
     public Button _dungeonBtn;
-
+   
     public void StartDialog()
     {
+        //Managers.Input.KeyAction = null;
+        //Managers.Input.MouseAction = null;
         StartCoroutine(DialogStart());
     }
 
@@ -18,20 +20,23 @@ public class DialogDungeonUI : MonoBehaviour
     {
         //첫 대사 시작전에 버튼을 숨기고 있다가
         _dungeonBtn.gameObject.SetActive(false);
-
+        //첫 대사 시작
         yield return new WaitUntil(() => _dialogSystem[0].UpdateDialog());
-
+        //버튼 활성화 하면서
         _dungeonBtn.gameObject.SetActive(true);
-
+        //두 번째 대사
         yield return new WaitUntil(() => _dialogSystem[1].UpdateDialog());
-
+        //그냥 다이얼로그를 종료할경우 버튼만 비활성화
         yield return new WaitForSeconds(0.2f);
         _dungeonBtn.gameObject.SetActive(false);
+        //Managers.Input.KeyAction = Managers.Input.Clear;
     }
 
+    //던전 UI 오픈 함수
     public void OpenDungeonUI()
     {
         _dungeonUI.SetActive(true);
         _dungeonBtn.gameObject.SetActive(false);
+        //Managers.Input.KeyAction = null;
     }
 }
