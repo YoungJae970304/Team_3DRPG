0774de4dd5fb17983b91bf0ceca11fa44fac45a9
youngjun!!! <jrys7754@gmail.com>
2024-10-16 23:32:06 +0900

    chore : 키액션 예외처리 진행중

--- Assets/02_Scripts/UI/New Folder/DialogActive.cs
+++ Assets/02_Scripts/UI/New Folder/DialogActive.cs
@@ -14,19 +14,13 @@ public class DialogActive : MonoBehaviour
         if (_isDialogOpen)
         {
             _originKeyAction = Managers.Input.KeyAction;
-            Logger.Log("키액션 저장 : " +_originKeyAction?.Method.Name);
-            Managers.Input.KeyAction = null;
-            Logger.Log("키액션 멈춤");
+            //Managers.Input.KeyAction = null;
         }
         else
         {
             if (_originKeyAction != null)
             {
                 Managers.Input.KeyAction = _originKeyAction;
-            }
-            else
-            {
-                Logger.Log("복구할 키 없음");
             }
         }
     }
