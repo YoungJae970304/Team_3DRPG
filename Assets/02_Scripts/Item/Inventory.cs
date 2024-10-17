acc09c12bf5b48a77cb7e30804e5fddbe05350bd
gray-spade <dltkdwns98@naver.com>
2024-10-17 16:38:24 +0900

    chore:주석

--- Assets/02_Scripts/Item/Inventory.cs
+++ Assets/02_Scripts/Item/Inventory.cs
@@ -39,10 +39,10 @@ public class Inventory : MonoBehaviour//인벤토리
 
     }
 
-    public bool InsertItem(Item item)//아이템 삽입 빈칸이있으면 빈칸으로 중복이있으면 합쳐짐
+    public int InsertItem(Item item)//아이템 삽입 빈칸이있으면 빈칸으로 중복이있으면 합쳐짐
     {
         Logger.Log(item.Data.Type.ToString());
-        bool result = ItemDick[item.Data.Type].Insert(item);
+        int result = ItemDick[item.Data.Type].Insert(item);
         GetItemAction?.Invoke();
 
         //아이템의 타입에 따라 타입에 맞는 그룹에 삽입한다
@@ -109,65 +109,63 @@ public class Inventory : MonoBehaviour//인벤토리
     }
     public class ItemGroup
     {//특정 타입의 아이템을 모아 관리하는 아이템 그룹
-        Item[] _items;
-        public int _maxSize;
-        List<ItemData.ItemType> _type = new List<ItemData.ItemType>();
-        public ItemGroup(int maxSize, int LimitSize, ItemData.ItemType[] types)
-        {
-            _items = new Item[LimitSize];
-            _maxSize = maxSize;
-            foreach (var type in types)
-            {
-                _type.Add(type);
-            }
-        }
-        public ItemGroup(int maxSize, int LimitSize, ItemData.ItemType type)
+        Item[] _items;              //아이템 배열
+        public int _maxSize;        //그룹의 크기
+        List<ItemData.ItemType> _type = new List<ItemData.ItemType>();//그룹이 담고있는 아이템들의 타입들
+        bool isFull = false;        //슬롯이 꽉찼는지 확인
+        public ItemGroup(int maxSize, int LimitSize, ItemData.ItemType type)//크기,한계크기,타입
         {
             _items = new Item[LimitSize];
             _maxSize = maxSize;
             _type.Add(type);
         }
-        public void AddGroup(ItemData.ItemType type)
+        public void AddGroup(ItemData.ItemType type)//타입추가
         {
             _type.Add(type);
         }
-        public bool Insert(Item item)
+        public int Insert(Item item)
         {//중복이 있으면 아이템의 개수를 늘리고 최대치를 넘겼다면 다음으로 넘어가며
          //중복이 없으면 빈칸에 아이템을 할당한다.
-
-            if (item is CountableItem)
+            
+            int overAmount = 1; ;
+            if (item is CountableItem)//쌓이는 아이템일때
             {
                 CountableItem countableItem = item as CountableItem;
+                //슬롯이 가득차면 수량 전부 리턴
+                if (isFull) { return countableItem.GetCurrentAmount(); }
                 for (int i = 0; i < _maxSize; i++)
                 {
 
-                    if (_items[i] == null || !(_items[i] is CountableItem)) { continue; }//빈칸이거나 1개가 최대인 아이템은 무시
+                    if (_items[i] == null || !(_items[i] is CountableItem)) { continue; }//빈칸이거나 쌓이지않는 아이템은 무시
                     if (_items[i].Data.ID == item.Data.ID)
                     {
-                        int overamount = ((CountableItem)_items[i]).AddAmount(countableItem._amount);
-                        if (overamount == 0) { return true; }
-                        else
-                        {
-                            countableItem.SetAmount(overamount);
-                        }
                         //아이템 갯수 줄어들고 저장된 아이템의 개수증가
                         //만약 받은 아이템 개수가 남았다면 continue
                         //아니면 return;
+                        overAmount = ((CountableItem)_items[i]).AddAmount(countableItem._amount);
+                        if (overAmount == 0) { return 0; }
+                        else
+                        {
+                            countableItem.SetAmount(overAmount);
+                        }
+                        
                     }
                 }
                 item = countableItem;
             }
-            for (int i = 0; i < _maxSize; i++)
+            //꽉찼는데 CountableItem이 아니면 1리턴
+            if (isFull) { return 1; }
+            for (int i = 0; i < _maxSize; i++)//빈칸을 찾아 아이템 설정
             {
 
                 if (_items[i] == null)
                 {
-                    _items[i] = item; return true;
+                    _items[i] = item; return 0;
                 }
             }
-
-
-            return false;
+            //아이템을 넣지 못했다면 꽉찼다는 표시 후 남은 량만큼 리턴
+            isFull = true;
+            return overAmount;
         }
         public Item GetItem(int index)
         {//인덱스로 아이템을 리턴
@@ -212,6 +210,7 @@ public class Inventory : MonoBehaviour//인벤토리
                 Item lastItme;
                 lastItme = _items[index];
                 _items[index] = null;
+                isFull = false;
                 return lastItme;
             }
             return null;
