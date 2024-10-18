a19f9f341f6257e767706fb7591453eb61901ac3
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-10-18 16:55:46 +0900

    style: 던전UI관련 스크립트 통합

--- Assets/02_Scripts/UI/Dungeon/DungeonUI.cs
+++ Assets/02_Scripts/UI/Dungeon/DungeonUI.cs
@@ -1,77 +1,117 @@
-using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DungeonUI : BaseUI
 {
-    enum DungeonUIElement
+    enum DungeonUIButton
     {
-        CloseBtn,
         EntryBtn,
-        SelectDungeonName,
-        SelectDungeonMainMonster,
-        SelectDungeonMonster,
-        SelectDungeonType,
     }
+    enum DungeonUIText
+    {
+        SelectDungeonName,
+    }
+    enum SelectDungeonType
+    {
+        Dungeon70001,
+        Dungeon70002,
+        Dungeon70003,
+        Dungeon70004,
+    }
+    enum DungeonUIImage
+    {
+        SelectDungeonMainMonster,
+    }
+    enum InDungeonMonster
+    {
+        Monster1,
+        Monster2,
+        Monster3,
+        Monster4,
+    }
+
     [Header("버튼 관련 변수")]
     SceneBtnController _sceneBtnController;
+    Dictionary<string, int> _buttonType = new Dictionary<string, int>();
+    [Header("던전 내 몬스터 이미지 관련 변수")]
+    List<Image> _indungeonMonsterImage = new List<Image>();
     [Header("Dungeon관련 변수")]
     public DataTableManager _dataTableManager;
     public DeongeonType _deongeonLevel;
     int _dungeonID;
     public GameObject _dungeonTypeview;
-    DungeonData _dungeonData;
+    //DungeonData _dungeonData;
     string _dungeonName;
     public int _monsterType1;
     public int _monsterType2;
     public int _monsterType3;
     [Header("UI이미지 관련 변수")]
-    public GameObject _mainMonster;
-    public TextMeshProUGUI _dungeonText;
     public GameObject _monsterImageType;
     public override void Init(Transform anchor)
     {
         base.Init(anchor);
+        //Logger.LogError("123213");
         //만약 던전UI가(이게 붙어있는 게임오브젝트가)없다면
         //시작 작업을 실행(버튼생성, 이 오브젝트 파괴불가, 우측 UI level_easy로 초기설정)
-    }
-    private void Awake()
-    {
         _dataTableManager = Managers.DataTable;
         _deongeonLevel = DeongeonType.Easy;
         _sceneBtnController = new SceneBtnController();
+        //여기에 button, 이미지 생성 들어갈거임 //필요한거 List에 버튼하고 이미지 담아두기
+        
+        MakeDungeUIElement();
 
-        Bind<Button>(typeof(DungeonUIElement));
-        Bind<Button>(typeof(DungeonUIElement));
 
-        GetButton((int)DungeonUIElement.EntryBtn).onClick.AddListener(() =>_sceneBtnController.OnClickSceneChangeBtn("03_Dungeon"));//여기에 입장 관련 함수가 들어감
-        GetButton((int)DungeonUIElement.CloseBtn).onClick.AddListener(() => CloseUI(this));//여기에 닫을때관련 함수가 들어가는듯
-    }
-
-    IEnumerator DungeonUIClose()
-    {
-        //이게 혼자인지 유효성검사 후 true가 되는 불변수를 false로 변경
-        //이 스크립트가 붙은 gameobject setactive false //이게 managers.ui.closeUI(this)인듯
-        //우측 UI전부다 easy난이도 일때로 변경
-        return null; // 오류 방지용 추후 변경이나 삭제
 
     }
-    public void MakeDungeonType()
+    public override void SetInfo(BaseUIData uiData)
     {
-        foreach (var dungeon in _dataTableManager._DungeonData)
+        base.SetInfo(uiData);
+        Logger.LogError("실행중");
+        Bind<Button>(typeof(DungeonUIButton));
+        Bind<TextMeshProUGUI>(typeof(DungeonUIText));
+        Bind<Button>(typeof(SelectDungeonType));
+        Bind<Image>(typeof(DungeonUIImage));
+
+        GetButton((int)DungeonUIButton.EntryBtn).onClick.AddListener(() => _sceneBtnController.OnClickSceneChangeBtn("Dungeon"));//여기에 입장 관련 함수가 들어감
+        DungeonButtonBind();
+        DungeonUITest(SwitchDungeonID((int)_deongeonLevel));
+    }
+
+    public void DungeonButtonBind()
+    {
+        GetButton((int)SelectDungeonType.Dungeon70001).onClick.AddListener(() => DungeonUITest(SwitchDungeonID(_buttonType[this.name])));
+        GetButton((int)SelectDungeonType.Dungeon70002).onClick.AddListener(() => DungeonUITest(SwitchDungeonID(_buttonType[this.name])));
+        GetButton((int)SelectDungeonType.Dungeon70003).onClick.AddListener(() => DungeonUITest(SwitchDungeonID(_buttonType[this.name])));
+        GetButton((int)SelectDungeonType.Dungeon70004).onClick.AddListener(() => DungeonUITest(SwitchDungeonID(_buttonType[this.name])));
+    }
+    public void MakeDungeUIElement()
+    {
+        foreach (var dungeon in _dataTableManager._DungeonData) //데이터 테이블 가져오기
         {
-            GameObject dungeonType;
-            dungeonType = Managers.Resource.Instantiate("UI/DeongeonType", _dungeonTypeview.transform);
-            dungeonType.GetComponent<TextMeshProUGUI>().text = dungeon.DungeonName;
-            Bind<Button>(typeof(DungeonUIElement));
+            GameObject dungeonType = Managers.Resource.Instantiate("UI/DeongeonType", _dungeonTypeview.transform);
+            // resource에 있는 instantiate호출. inspector창에 넣어놓은 부모 하위로 생성
+            dungeonType.name = $"Dungeon{dungeon.ID}";
+            //던전 이름 바꾸기 (Datatable의 ID값
+            dungeonType.GetComponentInChildren<TextMeshProUGUI>().text = dungeon.DungeonName;
+            _buttonType.Add(dungeonType.name, dungeon.Index);//딕셔너리에 오브젝트이름으로 키값설정. value는 index값 가져오기
 
-            //GetButton((int)DungeonUIElement.SelectDungeonType).onClick.AddListener(()=>);
+            int maxMonster = 4; //설정된 몬스터 최댓값설정 
+            for (int i = 1; i <= maxMonster; i++) //최댓값만큼 생성
+            {
+                GameObject monster = Managers.Resource.Instantiate("UI/MonsterImage", _monsterImageType.transform);
+                monster.name = $"Monster{i}";
+                Image monsterImage = monster.GetComponent<Image>();
+                _indungeonMonsterImage.Add(monsterImage); // 몬스터 이미지 리스트에 추가
+                monster.SetActive(false);
+            }
+
         }
 
 
     }
-    public void SwitchDungeonID(DeongeonType curType)
+    public int SwitchDungeonID(int index) //던전 ID받는 함수
     {
         Logger.LogError("실행확인");
         foreach (var dungeonType in _dataTableManager._DungeonData)
@@ -80,42 +120,15 @@ public class DungeonUI : BaseUI
             if (dungeonType == null)
             {
                 Logger.LogError("말이되냐");
-                return;
+
             }
-            switch (curType)
+            if (dungeonType.Index == index) //인덱스 값이 현재foreach로 돌아가고있는 index값과 같다면
             {
-                case DeongeonType.Easy:
-                    if (dungeonType.Index == (int)curType)
-                    {
-                        _dungeonID = dungeonType.ID;
-                        Logger.LogError($"{_dungeonID}값은 들어감");
-                    }
-                    break;
-                case DeongeonType.Normal:
-                    if (dungeonType.Index == (int)curType)
-                    {
-                        _dungeonID = dungeonType.ID;
-                        Logger.LogError($"{_dungeonID}얜 2번");
-                    }
-                    break;
-                case DeongeonType.Hard:
-                    if (dungeonType.Index == (int)curType)
-                    {
-                        _dungeonID = dungeonType.ID;
-                        Logger.LogError($"{_dungeonID}얜 3번");
-                    }
-                    break;
-                case DeongeonType.Boss:
-                    if (dungeonType.Index == (int)curType)
-                    {
-                        _dungeonID = dungeonType.ID;
-                        Logger.LogError($"{_dungeonID}얜 4번");
-                    }
-                    break;
-
+                _dungeonID = dungeonType.ID; //아이디는 그 당시에 들어갈 ID로 셋팅
+                Logger.LogError($"{_dungeonID}값은 들어감");
             }
-
         }
+        return _dungeonID;
     }
     public void DungeonUITest(int ID)
     {
@@ -129,41 +142,31 @@ public class DungeonUI : BaseUI
             }
 
             Logger.LogError($"{dungeonType.ID},{ID} 다른가?");
-            if (dungeonType.ID == ID)
+            if (dungeonType.ID == ID) //던전아이디가 돌아가고있는 foreach문의 id와 같다면
             {
-                _dungeonName = dungeonType.DungeonName;
-                _monsterType1 = dungeonType.MonsterType1;
-                _monsterType2 = dungeonType.MonsterType2;
-                _monsterType3 = dungeonType.MonsterType3;
-                Logger.LogError($"{_dungeonName}");
-                Logger.LogError($"{_monsterType1}");
-                Logger.LogError($"{_monsterType2}");
-                Logger.LogError($"{_monsterType3}");
+                _dungeonName = dungeonType.DungeonName; //세팅
+                _monsterType1 = dungeonType.MonsterType1; //세팅
+                _monsterType2 = dungeonType.MonsterType2; //세팅
+                _monsterType3 = dungeonType.MonsterType3; //세팅
                 break;
             }
         }
-        _dungeonText.text = _dungeonName;
-        Image mainMonster = _mainMonster.GetComponent<Image>();
-        mainMonster.sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{_monsterType1}");
-        //던전이름text = dungeonName;
+
+        GetText((int)DungeonUIText.SelectDungeonName).text = _dungeonName;
+        GetImage((int)DungeonUIImage.SelectDungeonMainMonster).sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/{_dungeonID}");//대표이미지가 던전아이디랬던거같음
+        //밑에 생성은 빠질거임 로드만 남을거임
         for (int i = _monsterType1; i <= _monsterType3; i++)
         {
-            GameObject dungeonMonsterType = Managers.Resource.Instantiate("UI/MonsterImage", _monsterImageType.transform);
-            Image dungeonMonsterImage = dungeonMonsterType.GetComponent<Image>();
-            dungeonMonsterImage.sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{i}"); //추후 경로 변경
-            //dungeonMonsterImage.rectTransform.localPosition = Vector3.zero;
-            Bind<Image>(typeof(DungeonUIElement));
+            if (i != 0)
+            {
+
+                GetImage((int)InDungeonMonster.Monster1).sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{i}");
+                GetImage((int)InDungeonMonster.Monster2).sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{i}");
+                GetImage((int)InDungeonMonster.Monster3).sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{i}");
+                GetImage((int)InDungeonMonster.Monster4).sprite = Managers.Resource.Load<Sprite>($"Prefabs/Enemy/Patern/{i}");
+            }
+
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
