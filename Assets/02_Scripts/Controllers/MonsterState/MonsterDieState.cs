bf0abc0da10341641a1fa53bc0ef2a9311850684
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-10-22 00:00:05 +0900

    chore: 몬스터 die상태 수정

--- Assets/02_Scripts/Controllers/MonsterState/MonsterDieState.cs
+++ Assets/02_Scripts/Controllers/MonsterState/MonsterDieState.cs
@@ -17,12 +17,17 @@ public class MonsterDieState : BaseState
         //_monster.GetComponent<BoxCollider>().enabled = false;
         _monster._monsterDrop.DropItemSelect(_monster._deongeonLevel, _monster.sample);//임시 설정 추후 던전에서 받아오도록 변경
         _monster.MakeItem();
-        _monster.Die(_monster.gameObject);
+        _monster.StartCoroutine(IvokeDie());
      
         // 영재 : 임시로 죽었을 때 게임매니저에서 제거하는 부분 추가
         Managers.Game._monsters.Remove(_monster);
     }
-   
+    public IEnumerator IvokeDie()
+    {
+        yield return new WaitForSeconds(2);
+        GameObject mob = _monster.gameObject;
+        _monster.Die(mob);
+    }
     public override void OnStateExit()
     {
         
