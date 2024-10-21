9ece84d75e3ee96ed7de5831036bf442d8ef62c0
youngjun!!! <jrys7754@gmail.com>
2024-10-21 23:54:50 +0900

    chore : NPC 대화 예외처리

--- Assets/02_Scripts/UI/Dialog/DungeonDialogUI.cs
+++ Assets/02_Scripts/UI/Dialog/DungeonDialogUI.cs
@@ -5,50 +5,33 @@ using UnityEngine.UI;
 
 public class DungeonDialogUI : BaseUI
 {
-    #region BIND
-    enum Buttons
-    {
-        YesBtn,
-    }
-
-    enum Texts
-    {
-        YesBtnTxt,
-        ExitBtnTxt,
-    }
-
-    #endregion
-
     public DialogSystem[] _dialogSystem;
 
     public override void Init(Transform anchor)
     {
         base.Init(anchor);
-        Bind<Button>(typeof(Buttons));
-        Bind<TextMeshProUGUI>(typeof(Texts));
         //대화 시작 시 모든 유아이 닫아버리기
         Managers.UI.CloseAllOpenUI();
         StartCoroutine(DungeonDialog());
+    }
+
+    public override void CloseUI(bool isCloseAll = false)
+    {
+        base.CloseUI(isCloseAll);
+        CloseUI(this);
     }
 
     IEnumerator DungeonDialog()
     {
         Managers.Game._isActiveDialog = true;
         Managers.Game._player._isMoving = false;
-        GetButton((int)Buttons.YesBtn).interactable = false;
-        GetText((int)Texts.YesBtnTxt).text = "던전 선택";
-        GetText((int)Texts.ExitBtnTxt).text = "아니?";
-        yield return new WaitForSeconds(0.2f);
-        GetButton((int)Buttons.YesBtn).interactable = true;
         yield return new WaitUntil(() => _dialogSystem[0].UpdateDialog());
-        GetButton((int)Buttons.YesBtn).onClick.RemoveAllListeners();
-        GetButton((int)Buttons.YesBtn).onClick.AddListener(() => { OpenDungeonUI(); });
         Managers.Game._isActiveDialog = false;
         Managers.Game._player._isMoving = true;
+        yield return new WaitForSeconds(0.2f);
         Managers.UI.CloseUI(this);
     }
 
-    #region 버튼 함수들
     //던전 UI 오픈 함수 버튼 클릭시 생성
     public void OpenDungeonUI()
     {
@@ -63,5 +46,4 @@ public class DungeonDialogUI : BaseUI
             Managers.UI.OpenUI<DungeonUI>(new BaseUIData());
         }
     }
-    #endregion
 }
--- Assets/02_Scripts/UI/Dialog/ShopDialogUI.cs
+++ Assets/02_Scripts/UI/Dialog/ShopDialogUI.cs
@@ -5,49 +5,33 @@ using UnityEngine.UI;
 
 public class ShopDialogUI : BaseUI
 {
-    #region BIND
-    enum Buttons
-    {
-        YesBtn,
-    }
-
-    enum Texts
-    {
-        YesBtnTxt,
-        ExitBtnTxt,
-    }
-
-    #endregion
-
     public DialogSystem[] _dialogSystem;
 
     public override void Init(Transform anchor)
     {
         base.Init(anchor);
-        Bind<Button>(typeof(Buttons));
-        Bind<TextMeshProUGUI>(typeof(Texts));
         //대화 시작 시 모든 유아이 닫아버리기
         Managers.UI.CloseAllOpenUI();
         StartCoroutine(ShopDialog());
     }
+
+    public override void CloseUI(bool isCloseAll = false)
+    {
+        base.CloseUI(isCloseAll);
+        CloseUI(this);
+    }
+
     IEnumerator ShopDialog()
     {
         Managers.Game._isActiveDialog = true;
         Managers.Game._player._isMoving = false;
-        GetButton((int)Buttons.YesBtn).interactable = false;
-        GetText((int)Texts.YesBtnTxt).text = "상점 이용";
-        GetText((int)Texts.ExitBtnTxt).text = "아니?";
-        yield return new WaitForSeconds(0.2f);
-        GetButton((int)Buttons.YesBtn).interactable = true;
         yield return new WaitUntil(() => _dialogSystem[0].UpdateDialog());
-        GetButton((int)Buttons.YesBtn).onClick.RemoveAllListeners();
-        GetButton((int)Buttons.YesBtn).onClick.AddListener(() => { OpenShopUI(); });
         Managers.Game._isActiveDialog = false;
         Managers.Game._player._isMoving = true;
+        yield return new WaitForSeconds(0.2f);
         Managers.UI.CloseUI(this);
     }
 
-    #region 버튼 함수들
     //던전 UI 오픈 함수 버튼 클릭시 생성
     public void OpenShopUI()
     {
@@ -62,7 +46,7 @@ public class ShopDialogUI : BaseUI
             BaseUIData uiData = new BaseUIData();
             ShopUIData shopData = uiData as ShopUIData;
             Managers.UI.OpenUI<ShopUI>(shopData);
+            Managers.UI.CloseUI(this);
         }
     }
-    #endregion
 }
