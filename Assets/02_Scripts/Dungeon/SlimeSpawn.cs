5a11c7754111ba154fdd5a3809fbe51e2474d504
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-10-22 16:34:25 +0900

    style: 몬스터 생성스타일 변환

---
+++ Assets/02_Scripts/Dungeon/SlimeSpawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawn : SpawnEnemy
+{
+    public override void Start()
+    {
+        base.Start();
+        MonsterSpawn(1);
+    }
+
+}
