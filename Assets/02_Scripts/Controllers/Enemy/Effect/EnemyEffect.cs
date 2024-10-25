9043881657e22210c13cebca2dcda7d61588d55f
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-10-25 16:53:29 +0900

    fix: 수정해야할거많음

--- Assets/02_Scripts/Controllers/Enemy/Effect/EnemyEffect.cs
+++ Assets/02_Scripts/Controllers/Enemy/Effect/EnemyEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyEffect : BaseUI
+public class EnemyEffect : BaseUI //monobihavior로 변경
 {
     public enum GoblemOrkEffects
     {
@@ -12,19 +12,30 @@ public class EnemyEffect : BaseUI
         MonsterHit,
         Count,
     }
-
-    private void Awake()
+    private void OnEnable()
     {
         Bind<ParticleSystem>(typeof(GoblemOrkEffects));
-        for(int i = 0; i < (int)GoblemOrkEffects.Count; i++)
+        EffectOff();
+    }
+
+    public void EffectOff()
+    {
+        for (int i = 0; i < (int)GoblemOrkEffects.Count; i++)
         {
             Get<ParticleSystem>(i).gameObject.SetActive(false);
+            Logger.LogError("이펙트 꺼짐");
         }
     }
-    public void MonsterAttack(GoblemOrkEffects name)
+    public void MonsterAttack(GoblemOrkEffects name, Transform playerTransform = null)
     {
         Get<ParticleSystem>((int)name).gameObject.SetActive(true);
-        //Get<ParticleSystem>((int)name).gameObject.transform.position = GetComponentInParent<Transform>().position;
+        Logger.LogError($"{Get<ParticleSystem>((int)name).gameObject.name}켜진 이펙트 이름임");
+        if (playerTransform != null) 
+        {
+            Get<ParticleSystem>((int)name).gameObject.transform.position = playerTransform.position;
+            Logger.LogError($"{Get<ParticleSystem>((int)name).gameObject.transform.position}바뀐위치임");
+        }
         Get<ParticleSystem>((int)name).Play();
+        Logger.LogError("이팩트 켜짐");
     }
 }
