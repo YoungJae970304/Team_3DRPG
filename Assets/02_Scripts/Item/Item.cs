2baead655609d394eb2de645d287c094e2835d1d
youngjun-111 <jrys7754@gmail.com>
2024-10-28 16:57:21 +0900

    chore : 파일 수정

--- Assets/02_Scripts/Item/Item.cs
+++ Assets/02_Scripts/Item/Item.cs
@@ -115,7 +115,7 @@ public class Item
         }
 
         Sprite icon = Managers.Resource.Load<Sprite>(iconPath);
-
+        Logger.Log($"{iconPath.ToString()} 불러옴");
         if (icon == null)
         {
             Logger.LogError($"icon 로드 실패: {iconPath}");
