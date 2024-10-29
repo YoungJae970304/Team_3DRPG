dc7d55bd4106aaf5032880bd61199b84f1db6685
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-10-29 16:49:26 +0900

    fix: OptionUI프리팹 수정

--- Assets/02_Scripts/UI/Option/OptionUI.cs
+++ Assets/02_Scripts/UI/Option/OptionUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class OptionUI : BaseUI
@@ -80,4 +81,5 @@ public class OptionUI : BaseUI
             Managers.UI.OpenUI<ConfirmUI>(confirmUIData);
         }
     }
+
 }
