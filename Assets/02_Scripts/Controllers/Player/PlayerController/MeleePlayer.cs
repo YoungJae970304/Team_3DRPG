7609349a657b7dd5c37a639ade00b1fd9d0c0fcf
YoungJae970304 <sksdl1643020@gmail.com>
2024-11-01 12:43:31 +0900

    fix: 근접 캐릭터 공격 시 이동이 같이 되는 문제 해결 / feat: 공격 시 피격된 적에게 피격 이펙트 생성

--- Assets/02_Scripts/Controllers/Player/PlayerController/MeleePlayer.cs
+++ Assets/02_Scripts/Controllers/Player/PlayerController/MeleePlayer.cs
@@ -15,6 +15,39 @@ public class MeleePlayer : Player
         Managers.Game._playerType = Define.PlayerType.Melee;
     }
 
+    public override void ApplyDamage(int damage)
+    {
+        if (_hitMobs.Count == 0) return;
+
+        foreach (var mob in _hitMobs)
+        {
+            if (mob.TryGetComponent<IDamageAlbe>(out var damageable))
+            {
+                damageable.Damaged(damage);
+                _effectController.HitEffectsOn("MeleeNormalHit", mob.transform);
+            }
+        }
+
+        _hitMobs.Clear();
+    }
+
+    public override void AreaDamage(float range, int damage)
+    {
+        Vector3 playerPos = Managers.Game._player.transform.position;
+
+        for (int i = 0; i < Managers.Game._monsters.Count; i++)
+        {
+            if (Vector3.Distance(playerPos, Managers.Game._monsters[i].transform.position) < range)
+            {
+                if (Managers.Game._monsters[i].TryGetComponent<IDamageAlbe>(out var damageable))
+                {
+                    damageable.Damaged(damage);
+                    _effectController.HitEffectsOn("MeleeNormalHit", Managers.Game._monsters[i].transform);
+                }
+            }
+        }
+    }
+
     public override void Attack()
     {
         ApplyDamage(_playerStatManager.ATK);
