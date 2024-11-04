9674866452480e2784fbf33273c45c1fef985a58
YoungJae970304 <sksdl1643020@gmail.com>
2024-11-04 14:07:53 +0900

    feat: NPC 모델링 추가

---
+++ Assets/02_Scripts/UI/Billboard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 적 체력 UI카메라에 보이게 하는 스크립트ㄴ
+public class Billboard : MonoBehaviour
+{
+    Transform target;
+
+    private void Start()
+    {
+        target = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        // 자기 자신의 방향을 카메라의 방향과 일치 시킨다
+        //transform.forward = target.forward;
+        transform.rotation = Quaternion.Euler(0f, target.rotation.eulerAngles.y, 0f);
+    }
+}
