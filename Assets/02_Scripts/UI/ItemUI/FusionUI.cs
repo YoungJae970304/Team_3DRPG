6e41783f2aa02864b03650c52fc4ba01a487e9c9
YoungJae970304 <sksdl1643020@gmail.com>
2024-11-21 13:54:21 +0900

    fix: 빌드 안되게하는 에디터 네임스페이스 제거

--- Assets/02_Scripts/UI/ItemUI/FusionUI.cs
+++ Assets/02_Scripts/UI/ItemUI/FusionUI.cs
@@ -2,7 +2,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using static UnityEditor.Progress;
+
 public class FusionUI : ItemDragUI
 {//아이템 합성 UI
     Inventory _inventory;
